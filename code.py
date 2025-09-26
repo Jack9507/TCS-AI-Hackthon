4de26bd8f1d577695d3b74ce3e5349e3aa5c56d5
JatinK <jatin998kk@gmail.com>
2025-09-26 10:03:19 +0530

    Initial commit code.py

---
+++ code.py
@@ -0,0 +1,15 @@
+import os
+
+def add_nums(x, y):
+    return x+y
+
+def divide_nums(a, b):
+    return a/b
+
+res1= add_nums(5, 6)
+print(res1)
+
+res2= divide_nums(256, 8)
+print(res2)
+
+print(os.getcwd())
